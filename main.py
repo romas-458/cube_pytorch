2d4cdbc388c7f5e851ee40190693691d50bb6175
roman <roman.maslii@peoly.com>
2021-10-28 23:04:16 +0300

    add epochs to main

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pandas as pd
 
 # examples = [okb_class, okt_class, nokb_class, nokt_class]
 
-def main(path_to_datajson, examples, root_dir, local_storage_dir):
+def main(path_to_datajson, examples, root_dir, local_storage_dir, epochs):
     ROOT_DIR = root_dir #"/home/roman/Projects/PreProjects/Cube_Project/Cube/train_pytorch"
     model_path = ROOT_DIR + "models_out"
     ai_default_model_path = os.path.join(ROOT_DIR, "models/cube_resnext101.pth")  # path to save model
@@ -23,7 +23,8 @@ def main(path_to_datajson, examples, root_dir, local_storage_dir):
         save_model_path=model_path,
         base_model_path=os.path.join(ROOT_DIR, ai_default_base_path),
         train_path=os.path.join(ROOT_DIR, local_storage_dir),
-        nok_threshold=ai_nok_threshold
+        nok_threshold=ai_nok_threshold,
+        epochs = epochs,
     )
 
     terminator = Terminator()
