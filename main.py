06dd4d2cdafcd43b8dac81816cec326f71c9624e
roman <roman.maslii@peoly.com>
2021-11-30 16:51:47 +0200

    add evaluation per object

--- main.py
+++ main.py
@@ -209,3 +209,38 @@ def evaluation(path_to_datajson, examples, root_dir, local_storage_dir, epochs, 
     # NOTE works only for binary case
     tn, fp, fn, tp = metrics.confusion_matrix(eval_df["label"].values, predictions).ravel()
     print('tn= ' + str(tn) + 'fp= ' + str(fp) + 'fn= '+ str(fn) + 'tp= '+ str(tp) )
+
+def evaluation_per_object(path_to_datajson, examples, root_dir, local_storage_dir, epochs, path_to_model):
+    ROOT_DIR = root_dir  # "/home/roman/Projects/PreProjects/Cube_Project/Cube/train_pytorch"
+    ai_default_model_path = os.path.join(ROOT_DIR, path_to_model)  # path to save model
+    model_path = os.path.join(ROOT_DIR, path_to_model)
+    ai_default_base_path = os.path.join(ROOT_DIR, "models/resnext101_32x8d-8ba56ff5.pth")  # imagenet weights
+    ai_nok_threshold = 0.5
+
+
+    pytorch_model = ClassifierModel(
+        save_model_path=ROOT_DIR + path_to_model,
+        base_model_path=os.path.join(ROOT_DIR, ai_default_base_path),
+        train_path=os.path.join(ROOT_DIR, local_storage_dir),
+        nok_threshold=ai_nok_threshold,
+        epochs = epochs,
+    )
+    # pytorch_model = ClassifierModel(
+    #     save_model_path=model_path,
+    #     # base_model_path=os.path.join(ROOT_DIR, path_to_model),
+    #     base_model_path=ai_default_model_path,
+    #     train_path=os.path.join(ROOT_DIR, local_storage_dir),
+    #     trained_model_path =
+    #     nok_threshold=ai_nok_threshold,
+    #     epochs=epochs,
+    # )
+    terminator = Terminator()
+    monitor = EvaluationMonitor()
+    eval_df, preds = pytorch_model.evaluate_from_csv_per_object(path_to_datajson, examples, monitor, terminator)
+
+    predictions = [0 if x < pytorch_model.nok_threshold else 1 for x in preds]
+    LOGGER.info("predictions len = " + str(len(predictions)) + 'eval len = ' + str(len(eval_df)))
+    assert len(predictions) == len(eval_df)
+    # NOTE works only for binary case
+    tn, fp, fn, tp = metrics.confusion_matrix(eval_df["label"].values, predictions).ravel()
+    print('tn= ' + str(tn) + 'fp= ' + str(fp) + 'fn= '+ str(fn) + 'tp= '+ str(tp) )
